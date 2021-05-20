5667d9e7f36b2a0f8f3ac4ed3307450b8060b7a5
김소은 <soeunkim0409@gmail.com>
2021-05-20 14:03:28 +0900

    기본 문법, bool 자료형

--- basic_types.py
+++ basic_types.py
@@ -23,7 +23,91 @@ def bool_ex(): #bool 자료형
     print("순차형:", bool([1,2,3]),bool([]))
     print("Map:", bool({"a":2}), bool({}))
     print("None:", bool(None)) # None: Java의 NULL과 비슷 --> 아무 것도 할당되어 있지 않은 상태를 의미
-    
+
+    #Circuit Break
+    print("CB or 1:", [1,2,3] or [])
+    #or: True or False --> 먼저 나온 True 반환 : 기본값 설정시 쓰는 방법
+    print("CB or 2:", [] or [1,2,3])
+    #False or True: 먼저나온 True 값인 (순서 상) [1,2,3]반환
+    print("CB and 1:", [1,2,3] and [4,5,6])
+    #cf. AND: 뒤의 값을 반환 (True&True -> T2 반환)
+    print("CB and 2:", [1,2,3] and [])
+    #True & False: False의 값을 반환 --> [] (=none) 값을 반환
+
+
+def integer_ex():  #정수형
+    print("==========정수형 연습")
+    a=23 #Literal
+    b=int(23) #타입 함수를 이용
+    print(a,"is",type(a))
+    print(b,"는 int의 객체?", isinstance(b,int))
+
+    #2진, 8진, 16진 정수 표현 가능
+    b= 0b1101 #2진: 0b
+    o=0o23  #8진: 0o
+    x=0xFF #16진: 0x
+    print(b,o,x)
+
+    #3.x에서는 int, long을 구분하지 않음
+    #long형의 저장크기인 64bit를 초과하는 정수도 입력 가능
+    i=2**2048
+    print(i)
+    print(i.bit_length()) #i 객체의 비트수 확인
+
+    #진법 변환 함수
+    print(bin(2021))
+    print(oct(2021))
+    print(hex(2021))
+
+def float_ex():
+    #실수형은 모두 float로 표기
+    print("==========실수형 연습")
+    a=3.14159 #리터럴로 선언
+    print(a,"is", type(a))
+    b=float(3.0) #타입 함수를 이용한 생성
+    print (b, "는 float의 객체인가?", isinstance(b,float))
+    #is_integer: 타입 판별이 아니라 값의 형태가 정수형(소숫점 아래 값이 없는지) 인지를 판별
+    print(a.is_integer(), b.is_integer())
+
+    #소수점 포함, e,E 지수 형태로 표현 가능
+    c=3e3 #3000
+    d=-2E-5 #-0.00002
+    print(c,d)
+    print(-2E-5==-0.00002)
+
+def complex_ex():
+    #복소수
+    print("=====복소수 연습")
+    # 복소수: 실수부 + 허수부J 형태
+    a=4+5j # 실수부 4, 허수부 5
+    print(a,"is",type(a))
+    b=7-2J #실수부 7, 허수부 -2
+    print(b, "complex의 객체?", isinstance(b,complex))
+
+    #복소수는 수치형 --> 산술연산 가능
+    print (a+b)
+
+    print(a,"의 실수부?", a.real)
+    print(a,"의 허수부?", a.imag)
+    print(a, "의 켤레 복소수?", a.conjugate())
+
+    #타입 함수를 이용한 복소수의 생성
+    c=7
+    d=3
+    e=complex(7,3)
+    print(e, "is", type(e))
+
+def builtin_math_function(): #내장 수치 함수
+    print(abs(-1)) #절댓값
+    print(int(3.14159)) #타입 함수를 이용한 타입의 변환
+    print(float(3)) #타입변환
+    print(complex(1)) # 타입변환
+    print(divmod(5,3)) #정수 나눗셈 (몫, 나머지)
+    print(pow(2,10))
 
 if __name__=="__main__":
-    bool_ex()
+#    bool_ex()
+#    integer_ex()
+#    float_ex()
+#    complex_ex()
+    builtin_math_function()
