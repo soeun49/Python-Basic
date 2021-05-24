8697b31f04e84cbd5f598d0410efe993d2db5dd0
김소은 <soeunkim0409@gmail.com>
2021-05-24 17:51:00 +0900

    qustion2

---
+++ quiz02_3.py
@@ -0,0 +1,22 @@
+def question3():
+
+    while x=input("Method:")
+        sum=0
+        if x=="d":
+           add=string(input("Amount:"))
+           sum+=add
+           print("Balance:",sum)
+           continue
+        if x=="w":
+           minus=string(input("Amount:"))
+           sum-=minus
+           print("Balance:",sum)
+           continue
+        if x=="q":
+           break
+    else:
+        print("?")
+
+
+if __name__ == "__main__":
+    question3()
